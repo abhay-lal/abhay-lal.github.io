ae3c3add7d44e6e74f081cc14b20d4a09cdd5b3d
Abhay <abhaylal@icloud.com>
2025-11-19 07:15:26 -0800

    res add

--- src/components/Home.js
+++ src/components/Home.js
@@ -169,7 +169,7 @@ const Home = () => {
                 Contact <i className="uil uil-message button__icon"></i>
               </a>
               <a
-                href="https://drive.google.com/file/d/1rwsAUzQDcaUS09s8ajCAgt9r3eT1iuBv/view?usp=sharing"
+                href="https://drive.google.com/file/d/1GsdrzVfbHQhN-uae1LuQfqFN4DmwU5Jq/view?usp=sharing"
                 className="button button--flex"
                 target="_blank"
                 rel="noopener noreferrer"
